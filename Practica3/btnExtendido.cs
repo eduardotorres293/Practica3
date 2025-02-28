661d50c5d0be34faad6d6fd72dc2bc0db42a84bd
pc <al23760375@ite.edu.mx>
2025-02-27 16:45:05 -0800

    Agregar archivos de proyecto.

---
+++ Practica3/btnExtendido.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica3
+{
+    public partial class btnExtendido: UserControl
+    {
+        public btnExtendido()
+        {
+            InitializeComponent();
+            button1.MouseEnter += Button1_MouseEnter;
+            button1.MouseLeave += Button1_MouseLeave;
+            button1.MouseDown += Button1_MouseDown;
+        }
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.LightGray;
+        }
+
+        private void Button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = default(Color);
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && e.Clicks == 2)
+            {
+                DialogResult confirmacion = MessageBox.Show("Se realizó una acción de doble click, ¿Correcto?", "Evento de doble click", MessageBoxButtons.YesNo);
+                if (confirmacion == DialogResult.Yes)
+                {
+                    MessageBox.Show("//PlaceholderYes");
+                }
+                else
+                {
+                    MessageBox.Show("//PlaceholderNo");
+                }
+            }
+        }
+    }
+}
---
+++ Practica3/txtBoxExtendido.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica3
+{
+    public partial class txtBoxExtendido : UserControl
+    {
+        public txtBoxExtendido()
+        {
+            InitializeComponent();
+            textBox1.KeyPress += TxtInput_KeyPress;
+        }
+
+        public enum valor
+        {
+            NumbersOnly, WordsOnly, Both
+        }
+
+        private valor modoElegido = valor.Both;
+
+        [Category("Behavior")]
+        [Description("Define si solo se aceptan números, letras o ambos")]
+        [DefaultValue(valor.Both)]
+
+        // Esta parte la deje en español para que sea fácil de diferenciar entre las otras
+        public valor ModoDeEntrada
+        {
+            get { return modoElegido; }
+            set { modoElegido = value; }
+        }
+
+        private void TxtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool esInvalido = false;
+            switch (modoElegido)
+            {
+                case valor.NumbersOnly:
+                    if ((e.KeyChar >= 65 && e.KeyChar <= 122) && !char.IsControl(e.KeyChar))
+                    {
+                        e.Handled = true;
+                        esInvalido = true;
+                    }
+                    break;
+                case valor.WordsOnly:
+                    if (!(e.KeyChar >= 65 && e.KeyChar <= 122) && !char.IsControl(e.KeyChar))
+                    {
+                        e.Handled = true;
+                        esInvalido = true;
+                    }
+                    break;
+                case valor.Both:
+                    break;
+            }
+            if (esInvalido)
+            {
+                textBox1.BackColor = Color.LightPink;
+                textBox1.BorderStyle = BorderStyle.FixedSingle;
+            }
+            else
+            {
+                textBox1.BackColor = default(Color);
+                textBox1.BorderStyle = BorderStyle.Fixed3D;
+            }
+        }
+    }
+}
